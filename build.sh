e11279b53d507632a6b8b32220055d5c9fb960a8
powcoder <powcoder@163.com>
2022-12-06 21:40:36 +0800

    CS6340 Lab 4 CBI

---
+++ build.sh
@@ -0,0 +1,31 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/bin/bash
+
+#mkdir build
+[ ! -d "build" ] && mkdir build
+cd build
+cmake ..
+make clean
+make
+
+cd ../test/
+#mkdir fuzz_output
+[ ! -d "fuzz_output" ] && mkdir fuzz_output
+make
+timeout 10 ../build/fuzzer ./fuzz1 fuzz_input fuzz_output 10
+../build/cbi ./fuzz1 fuzz_output
+
+#Full run
+clang -emit-llvm -S -fno-discard-value-names -c fuzz1.c -g
+opt -load ../build/InstrumentPass.so -Instrument -S fuzz1.ll -o fuzz1.instrumented.ll
+opt -load ../build/CBIInstrumentPass.so -CBIInstrument -S fuzz1.instrumented.ll -o fuzz1.cbi.instrumented.ll
+clang -o fuzz1 ../lib/runtime.c fuzz1.cbi.instrumented.ll
+
+exit 
---
+++ src/CBIInstrument.cpp
@@ -0,0 +1,42 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+
+#include "CBIInstrument.h"
+
+using namespace llvm;
+
+namespace instrument {
+
+static const char *CBIBranchFunctionName = "__cbi_branch__";
+static const char *CBIReturnFunctionName = "__cbi_return__";
+
+/*
+ * Implement instrumentation for the branch scheme of CBI.
+ */
+void instrumentCBIBranches(Module *M, Function &F, BranchInst &I) {
+  /* Add your code here */
+}
+
+/*
+ * Implement instrumentation for the return scheme of CBI.
+ */
+void instrumentCBIReturns(Module *M, Function &F, CallInst &I) {
+  /* Add your code here */
+}
+
+bool CBIInstrument::runOnFunction(Function &F) {
+  /* Add your code here */
+  return true;
+}
+
+char CBIInstrument::ID = 1;
+static RegisterPass<CBIInstrument> X("CBIInstrument",
+                                     "Instrumentations for CBI", false, false);
+
+} // namespace instrument
